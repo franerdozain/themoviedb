2f3d28d743648a65260df307163dc421823ef4c2
Fran <franciscoerdozain22@gmail.com>
2023-07-09 23:18:27 -0300

    refactor Api requests component

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,87 +1,46 @@
 const BASE_URL = "https://api.themoviedb.org/3";
 const BEARER_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlODQ1MTg4OWMzNTAyYThlZjQ5OGFjYTY4MDhkODgzZCIsInN1YiI6IjY0OWFkMjlkMGU1YWJhMDExYzg5YjIyNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xrsHhcJLB_sS0vnmofaEfdW2IRtoeOvD_X4uXgMjFqc";
 
-export async function getMovies () {
+const getData = async (url) => {
     try {
-        const response = await fetch(`${BASE_URL}/discover/movie`, {
+        const response = await fetch(url, {
             headers: {
                 "Authorization": `Bearer ${BEARER_TOKEN}`
             }
-        });
-        const data = await response.json();        
-        return data.results;
+        });        
+        const data = await response.json();     
+        return data;    
     } catch (error) {
-        console.log("Error: ", error);
+        console.log("Error: ", error)
     }
+}
+
+export async function getMovies() {
+    const data = await getData(`${BASE_URL}/discover/movie`)
+    return data.results;
 }
 
 export async function getTvShows () {
-    try {
-        const response = await fetch(`${BASE_URL}/discover/tv`, {
-            headers: {
-                "Authorization": `Bearer ${BEARER_TOKEN}`
-            }
-        });
-        const data = await response.json();
-        return data.results;
-    } catch (error) {
-        console.log("Error: ", error);
-    }
+    const data = await getData(`${BASE_URL}/discover/tv`)
+    return data.results;
 }
 
-
 export async function getGenres (type) {
-    try {
-        const response = await fetch(`${BASE_URL}/genre/${type}/list`, {
-            headers: {
-                "Authorization": `Bearer ${BEARER_TOKEN}`
-            }
-        });
-        const data = await response.json();        
-        return data.genres;
-    } catch (error) {
-        console.log("Error: ", error);
-    }
+    const data = await getData(`${BASE_URL}/genre/${type}/list`)
+    return data.genres;
 }
 
 export async function getTitlesByGenre (type, genreId) {
-    try {
-        const response = await fetch(`${BASE_URL}/discover/${type}?&with_genres=${genreId}`, {
-            headers: {
-                "Authorization": `Bearer ${BEARER_TOKEN}`
-            }
-        });
-        const data = await response.json();        
-        return data.results;
-    } catch (error) {
-        console.log("Error: ", error);
-    }
+    const data = await getData(`${BASE_URL}/discover/${type}?&with_genres=${genreId}`)
+    return data.results;
 }
 
 export async function getTitleDetails (type, id) {
-    try {
-        const response = await fetch(`${BASE_URL}/${type}/${id}`, {
-            headers: {
-                "Authorization": `Bearer ${BEARER_TOKEN}`
-            }
-        });
-        const data = await response.json();
-        return data 
-    } catch (error) {
-        console.log("Error: ", error);
-    }
+    const data = await getData(`${BASE_URL}/${type}/${id}`)
+    return data;
 }
 
 export async function getSearchedTitle (type, title) {
-    try {
-        const response = await fetch(`${BASE_URL}/search/${type}?&query=${title}`, {
-            headers: {
-                "Authorization": `Bearer ${BEARER_TOKEN}`
-            }
-        });
-        const data = await response.json();        
-        return data.results;
-    } catch (error) {
-        console.log("Error: ", error);
-    }
+    const data =  await getData(`${BASE_URL}/search/${type}?&query=${title}`)
+    return data.results;
 }
